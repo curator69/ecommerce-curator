df5b4127f43586a5534af1a0d2b63d0ebd21f8eb
curator69 <75662004+curator69@users.noreply.github.com>
2022-11-11 21:05:51 +0530

    completed layput component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Head from "next/head";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div>
+      <div className="layout">
+        <Head>
+          <title>Curator</title>
+        </Head>
+        <header>
+          <Navbar></Navbar>
+        </header>
+        <main className="main-container">Empty</main>
+        <footer>
+          <Footer></Footer>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
